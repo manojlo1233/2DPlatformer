17f5ba49e6b1e2c9aa0b0440095aa3f12a3e1c6c
manojlo1233 <milan.manojlo00@gmail.com>
2025-04-15 12:19:01 +0200

    Added camera movement

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform previousRoom;
+    public Transform nextRoom;
+    public CameraController cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+            {
+                cam.MoveToNewRoom(nextRoom);
+            }
+            else
+            {
+                cam.MoveToNewRoom(previousRoom);
+            }
+        }
+    }
+}
